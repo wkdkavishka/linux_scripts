0c4ffe694aace76ac6cb4bf2fadf8b854e6d93c3
Dumindu Kavishka <71714631+wkdkavishka@users.noreply.github.com>
2023-02-16 14:30:07 +0530

    Add files via upload

---
+++ utils/krunner.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Remove the key mapping from the Application Launcher.
+kwriteconfig5 --file kwinrc --group ModifierOnlyShortcuts --key Meta ""
+# Open KRunner instead.
+kwriteconfig5 --file kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.krunner,/App,,toggleDisplay"
