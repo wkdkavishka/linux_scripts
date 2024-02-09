1b8ab90eb1fed6a9291a7309414d3b6978afb2c8
WKDKavishka <w.k.d.kavishka@outlook.com>
2024-02-09 12:53:04 +0530

    Chaged the intel-undervolt install process

---
+++ rclone/rclone.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+# ---------
+rclone sync 'One Drive': '/mnt/storage-500/Cloud Storage/One Drive'
+
+rclone sync 'Google Drive Master': '/mnt/storage-500/Cloud Storage/Google Drive Master'
+
+rclone sync 'Google Drive': '/mnt/storage-500/Cloud Storage/Google Drive'
---
+++ rclone/sync-google_photos.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+# ---------
+clone sync 'Google Photos':media/by-day '/mnt/storage-500/Cloud Storage/Google Photos/'  --verbose -P
