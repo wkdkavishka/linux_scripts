d81d74e694eb628fb05cf393b13daad1fb3449d8
wkdkavishka <dkmaxhome@gmail.com>
2024-07-26 11:22:45 +0530

    some quality improvements

--- rclone/sync-google_photos.sh
+++ rclone/sync-google_photos.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # ---------
-clone sync 'Google Photos':media/by-day '/mnt/storage-500/Cloud Storage/Google Photos/'  --verbose -P
+rclone sync 'Google Photos':media/by-day '/mnt/Storage-500/Cloud Storage/Google Photos/'  --verbose -P
--- utils/krunner.sh
+++ utils/krunner.sh
@@ -3,3 +3,6 @@
 kwriteconfig5 --file kwinrc --group ModifierOnlyShortcuts --key Meta ""
 # Open KRunner instead.
 kwriteconfig5 --file kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.krunner,/App,,toggleDisplay"
+
+
+killall plasmashell&&kstart5 plasmashell
