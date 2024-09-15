aff96f5bd2f39288a15c1134aa6898c096b933e8
wkdkavishka <w.k.d.kavishka@outlook.com>
2024-09-15 21:10:29 +0530

    change rclone remote name from Google Photos to google-photos

--- rclone/rclone.sh
+++ rclone/rclone.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 # ---------
-rclone sync 'One Drive Master': '/mnt/storage-500/Cloud Storage/One Drive'
+# rclone sync 'One Drive Master': '/mnt/storage-500/Cloud Storage/One Drive'
+#
+# rclone sync 'Google Drive Master': '/mnt/storage-500/Cloud Storage/Google Drive Master'
+#
+# rclone sync 'Google Drive': '/mnt/storage-500/Cloud Storage/Google Drive'
 
-rclone sync 'Google Drive Master': '/mnt/storage-500/Cloud Storage/Google Drive Master'
-
-rclone sync 'Google Drive': '/mnt/storage-500/Cloud Storage/Google Drive'
+rclone sync 'google-photos':media/by-day '/mnt/Storage/Cloud Storage/Google Photos/'  --verbose -P
--- rclone/sync-google_photos.sh
+++ rclone/sync-google_photos.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # ---------
-rclone sync 'Google Photos':media/by-day '/mnt/Storage-500/Cloud Storage/Google Photos/'  --verbose -P
+rclone sync 'google-photos':media/by-day '/mnt/Storage/Cloud Storage/Google Photos/'  --verbose -P
